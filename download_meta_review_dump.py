9c3585b0e4000a419d52d2b4f7d41366b5a54e88
Steffen Wenz <swenz@trustyou.net>
2017-05-29 14:19:20 +0200

    Update to boto3

--- download_meta_review_dump.py
+++ download_meta_review_dump.py
@@ -3,13 +3,12 @@
 Demonstration of how to download a complete meta-review API dump.
 """
 
-from getpass import getpass
 import logging
 import os.path
 import re
 import sys
 
-import boto
+import boto3
 
 def parse_args():
 	from argparse import ArgumentParser
@@ -17,15 +16,15 @@ def parse_args():
 	argp.add_argument("dest_folder", help="Existing empty folder where files should be stored")
 	return argp.parse_args()
 
-def download_latest(aws_access_key, aws_secret_key, dest_folder):
+def download_latest(dest_folder):
 	
 	logging.info("Connecting to Amazon S3")
 
-	# Connect to Amazon S3 using credentials.
-	conn = boto.connect_s3(aws_access_key, aws_secret_key)
+	# Connect to S3
+	s3 = boto3.resource("s3")
 
 	# Select the bucket containing dumps of the TrustYou API.
-	bucket = conn.get_bucket("trustyou-api")
+	bucket = s3.Bucket("trustyou-api")
 
 	# The trustyou-api bucket contains snapshots of the TrustYou API at
 	# different points in time. They are organized in folders whose name is
@@ -34,8 +33,8 @@ def download_latest(aws_access_key, aws_secret_key, dest_folder):
 	logging.info("Looking for latest complete dump")
 	
 	latest_date = None
-	for key in bucket.list(prefix="meta-review/"):
-		done_file_match = re.match("^meta-review/([^/]+)/done$", key.key)
+	for object_summary in bucket.objects.filter(Prefix="meta-review/"):
+		done_file_match = re.match("^meta-review/([^/]+)/done$", object_summary.key)
 		if done_file_match:
 			date = done_file_match.group(1)
 			if latest_date is None or date > latest_date:
@@ -49,23 +48,26 @@ def download_latest(aws_access_key, aws_secret_key, dest_folder):
 	if not os.path.exists(target_folder):
 		os.makedirs(target_folder)
 
-	for key in bucket.list(prefix="meta-review/{}/".format(latest_date)):
-		basename = key.key.split("/")[-1]
+	for object_summary in bucket.objects.filter(Prefix="meta-review/{}/".format(latest_date)):
+		basename = object_summary.key.split("/")[-1]
 		if basename == "done":
 			continue
 		local_path = os.path.join(target_folder, basename)
 		if os.path.exists(local_path):
 			logging.debug("- Skipping %s, already downloaded", local_path)
 			continue
-		logging.debug("- Downloading %s", key.key)
+		logging.debug("- Downloading %s", object_summary.key)
 		local_tmp_path = local_path + "_tmp"
-		key.get_contents_to_filename(local_tmp_path)
+		object = object_summary.Object()
+		object.download_file(local_tmp_path)
 		os.rename(local_tmp_path, local_path)
 
 if __name__ == "__main__":
 
 	logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
-	logging.getLogger("boto").setLevel(logging.INFO)
+	logging.getLogger("botocore").setLevel(logging.CRITICAL)
+	logging.getLogger("boto3").setLevel(logging.CRITICAL)
+	logging.getLogger("s3transfer").setLevel(logging.CRITICAL)
 	
 	args = parse_args()
 	
@@ -73,9 +75,6 @@ if __name__ == "__main__":
 
 	assert os.path.exists(args.dest_folder) and os.path.isdir(args.dest_folder), "Destination folder does not exist!"
 
-	aws_access_key = getpass("Please enter your AWS access key and press Enter (characters not visible): ")
-	aws_secret_key = getpass("AWS secret key: ")
-
-	download_latest(aws_access_key, aws_secret_key, args.dest_folder)
+	download_latest(args.dest_folder)
 
 	logging.info("Done!")
